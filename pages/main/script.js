69cfe8385e0055a8900222893e2cee9eb3a9252e
Darek <gawlikdaarek@gmail.com>
2022-12-15 17:27:31 +0100

    feat: add functions from week 4

--- pages/main/script.js
+++ pages/main/script.js
@@ -5,6 +5,9 @@ const bodyShadow = document.querySelector('.body_shadow')
 const navPopUpHeader = document.querySelector('.nav_popup')
 const navPopupCross = document.querySelector('.nav_popup_cross')
 
+const RIGHT_DIRECTION = 'next'
+const LEFT_DIRECTION = 'prev'
+
 /// HEADER FUNCTION
 
 const showPopUpHeader = () => {
@@ -30,41 +33,84 @@ navPopupCross.addEventListener('click', closePopUpHeader)
 // CAROUSEL PETS FUNCTION
 
 const buttonsPets = document.querySelectorAll('[data-carousel-button]')
-const gap = 16
+const carousel = document.querySelector('.carousel')
 
-buttonsPets.forEach(button => {
-	button.addEventListener('click', () => {
-		const slides = Array.from(document.querySelectorAll('.animal_card'))
-		function shuffle(array) {
-			let currentIndex = array.length
-			let randomIndex
+let width = carousel.offsetWidth
+window.addEventListener('resize', e => (width = carousel.offsetWidth))
 
-			while (currentIndex != 0) {
-				randomIndex = Math.floor(Math.random() * currentIndex)
-				currentIndex--
-				;[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
-			}
-			return array
+const changeOrder = direction => {
+	;(function createNewCards() {
+		for (let i = 0; i < 6; i++) {
+			const newAnimalCard = document.createElement('div')
+			carousel.appendChild(newAnimalCard)
+			newAnimalCard.classList.add('animal_card')
 		}
-		shuffle(slides)
-		const sldesAfterFunction = document.getElementsByClassName('animals_wrapper')
-		sldesAfterFunction[0].replaceChildren(...slides)
+	})()
+
+	let slides = Array.from(document.querySelectorAll('.animal_card'))
+	function shuffle(array) {
+		let currentIndex = array.length
+		let randomIndex
+		while (currentIndex != 0) {
+			randomIndex = Math.floor(Math.random() * currentIndex)
+			currentIndex--
+			;[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
+		}
+		return array
+	}
+
+	shuffle(slides)
+
+	const sldesAfterFunction = document.getElementsByClassName('carousel')
+	const parent = document.getElementById('carousel_container')
+	const firstImage = parent.children[0]
+	slides.forEach(item => {
+		if (direction === RIGHT_DIRECTION) sldesAfterFunction[0].append(item)
 	})
+
+	if (direction === LEFT_DIRECTION) {
+		const nowydiv = document.createElement('div')
+		nowydiv.classList.add('leftdiv')
+		for (let i = 0; i < 6; i++) {
+			const nowydiv = document.createElement('div')
+			nowydiv.classList.add('animal_card')
+			parent.insertBefore(nowydiv, firstImage)
+			console.log(firstImage)
+		}
+	}
+
+	// document.querySelectorAll('.animal_card').forEach(card => {
+	// 	console.log('hej')
+	// 	card.remove()
+	// })
+}
+
+document.querySelector('[data-carousel-button="next"]').addEventListener('click', e => {
+	console.log('klikam w prawo')
+	changeOrder(RIGHT_DIRECTION)
+	carousel.scrollBy(1000, 0)
+})
+document.querySelector('[data-carousel-button="priev"]').addEventListener('click', e => {
+	console.log('klikam w lewo')
+	changeOrder(LEFT_DIRECTION)
+	carousel.scrollBy(-1000, 0)
 })
 
-// const carousel = document.querySelector('.carousel')
-// const prev = document.querySelector('.btn_arrow_right')
-// const next = document.querySelector('.btn_arrow_left')
 
-// next.addEventListener('click', e => {
-// 	carousel.scrollBy(width + gap, 0)
-// 	if (carousel.scrollWidth !== 0) {
-// 		prev.style.display = 'flex'
-// 	}
-// 	if (carousel.scrollWidth - width - gap <= carousel.scrollLeft + width) {
-// 		next.style.display = 'none'
-// 	}
-// })
 
-// let width = carousel.offsetWidth
-// window.addEventListener('resize', e => (width = carousel.offsetWidth))
+for (let item of data) {
+	output += `
+	<div class="user_card fade" id="${index}">
+		<div class="user_info">
+			<img class="user_icon" src="${item.img}" alt="user_icon">
+			<div class="user_text">
+				<h4>${item.name}</h4>
+				<p>${item.location}&ensp;•&ensp;${item.date}</p>
+			</div>
+		</div>
+		<div class="user_feedback">
+			<p>${item.description}</p>
+		</div>
+	</div>`;
+	index++;
+}
